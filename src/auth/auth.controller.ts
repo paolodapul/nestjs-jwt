5e7ea3484418c55341bf67dbb0fe3d056ca68823
Paolo Dapul <paolodapultech@gmail.com>
2025-01-03 12:37:32 +0800

    feat: implement JWT generation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpException, Post } from '@nestjs/common';
+import { AuthPayloadDto } from './dto/auth.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Post('login')
+    login(@Body() authPayload: AuthPayloadDto) {
+        const user = this.authService.validateUser(authPayload);
+        if (!user) throw new HttpException('Invalid credentials', 401);
+        return user;
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { AuthPayloadDto } from './dto/auth.dto';
+import { JwtService } from '@nestjs/jwt';
+
+const fakeUsers = [
+    {id: 1, username: "John", password: 'password'},
+    {id: 2, username: "Jane", password: 'password123'}]
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(private jwtService: JwtService) {}
+
+    validateUser({username, password}: AuthPayloadDto) {
+        const findUser = fakeUsers.find((user) => user.username === username)
+        if (!findUser) return null;
+        if (password === findUser.password) {
+            const { password, ...user } = findUser
+            return this.jwtService.sign(user)
+        }
+    }
+}
